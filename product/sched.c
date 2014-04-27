de06d43999d542a3f80182e8011ce774ed6f4330
wzhang23 <alievezhang@gmail.com>
2014-04-27 16:06:05 +0800

    Enable the statistics task. Add the OS CPU usage and Stack check functionality. Refactoring the sched.c.

--- product/sched.c
+++ product/sched.c
@@ -11,13 +11,29 @@
 #include "includes.h"	/* user top header file */
 #include "stdio.h"
 
+
 /*
 *********************************************************************************************************
-*                                       CONSTANTS DEFINITION
-*   i.e. task's stack size
-*        task's priority
-*        task's ID
-*        etc ...
+*                                       TYPES DEFINITION
+*********************************************************************************************************
+*/
+/*
+ * Define the struct of the task's parameters
+ */
+typedef struct
+{
+	INT32U id;
+	INT8U prio;
+	void ((*task)(void*));
+	OS_STK * ptos;
+	OS_STK * pbos;
+	INT32U stk_size;
+	OS_STK_DATA stk_chk_data;
+} TASK_PARA_ST;
+
+/*
+*********************************************************************************************************
+*                                       MACROS DEFINITION
 *********************************************************************************************************
 */
 #define TASK_STK_SIZE	512
@@ -42,6 +58,8 @@
 #define	TASK_1KMS_PRIO		7
 #define TASK_STK_CHK_PRIO 8
 
+#define TASK_STACK_CHECK_ENABLE
+
 /*
 *********************************************************************************************************
 *                                         Global VARIABLES
@@ -56,7 +74,9 @@ OS_STK  Task50MSStk[TASK_STK_SIZE];
 OS_STK  Task100MSStk[TASK_STK_SIZE];
 OS_STK  Task500MSStk[TASK_STK_SIZE];
 OS_STK  Task1KMSStk[TASK_STK_SIZE];
+#ifdef TASK_STACK_CHECK_ENABLE
 OS_STK  TaskStkChkStk[TASK_STK_SIZE];
+#endif
 
 /*
 *********************************************************************************************************
@@ -73,8 +93,30 @@ OS_STK  TaskStkChkStk[TASK_STK_SIZE];
        void Task100MS(void *);
        void Task500MS(void *);
        void Task1KMS(void *);
+#ifdef TASK_STACK_CHECK_ENABLE
        void TaskStkChk(void *);
+#endif
 static void TaskStartCreateTasks(void);
+
+/*
+*********************************************************************************************************
+*                                         GLOBAL
+*********************************************************************************************************
+*/
+TASK_PARA_ST task_para[] =
+{
+	{0, 0, TaskStart, &TaskStartStk[TASK_STK_SIZE-1], &TaskStartStk[0], TASK_STK_SIZE, {0,0}},
+	{1, 1, Task1MS, &Task1MSStk[TASK_STK_SIZE-1], &Task1MSStk[0], TASK_STK_SIZE, {0,0}},
+	{2, 2, Task10MS, &Task10MSStk[TASK_STK_SIZE-1], &Task10MSStk[0], TASK_STK_SIZE, {0,0}},
+	{3, 3, Task20MS, &Task20MSStk[TASK_STK_SIZE-1], &Task20MSStk[0], TASK_STK_SIZE, {0,0}},
+	{4, 4, Task50MS, &Task50MSStk[TASK_STK_SIZE-1], &Task50MSStk[0], TASK_STK_SIZE, {0,0}},
+	{5, 5, Task100MS, &Task100MSStk[TASK_STK_SIZE-1], &Task100MSStk[0], TASK_STK_SIZE, {0,0}},
+	{6, 6, Task500MS, &Task500MSStk[TASK_STK_SIZE-1], &Task500MSStk[0], TASK_STK_SIZE, {0,0}},
+	{7, 7, Task1KMS, &Task1KMSStk[TASK_STK_SIZE-1], &Task1KMSStk[0], TASK_STK_SIZE, {0,0}},
+#ifdef TASK_STACK_CHECK_ENABLE
+	{8, 8, TaskStkChk, &TaskStkChkStk[TASK_STK_SIZE-1], &TaskStkChkStk[0], TASK_STK_SIZE, {0,0}},
+#endif
+};
 
 /*
 *********************************************************************************************************
@@ -83,23 +125,15 @@ static void TaskStartCreateTasks(void);
 */
 void app(void) 
 {
-	OS_STK *ptos;
-	OS_STK *pbos;
-	INT16U size;
-
 	OSInit();
 	
-	ptos = &TaskStartStk[TASK_STK_SIZE-1];
-	pbos = &TaskStartStk[0];
-	size = TASK_STK_SIZE;
-	
-	OSTaskCreateExt(TaskStart,
+	OSTaskCreateExt(task_para[TASK_START_ID].task,
 	                (void *)0,
-	                ptos,
-	                TASK_START_PRIO,
-	                TASK_START_ID,
-	                pbos,
-	                size,
+	                task_para[TASK_START_ID].ptos,
+	                task_para[TASK_START_ID].prio,
+	                task_para[TASK_START_ID].id,
+	                task_para[TASK_START_ID].pbos,
+	                task_para[TASK_START_ID].stk_size,
 	                (void *)0,
 	                OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
 	                
@@ -131,6 +165,8 @@ void TaskStart(void * pdata)
 	TaskStartCreateTasks();         /* Create Tasks */
 	
 	for(;;) {
+		printf("OS context switch counter:%d\r\n", OSCtxSwCtr);
+		printf("OS CPU Usage:%d%%\r\n", OSCPUUsage);
 		OSCtxSwCtr = 0;
 		OSTimeDly(OS_TICKS_PER_SEC);
 	}
@@ -143,78 +179,21 @@ void TaskStart(void * pdata)
 */
 static void TaskStartCreateTasks(void)
 {
-    OSTaskCreateExt(Task1MS,
+	INT8U fl_index = 1; /* Task start had been created */
+	INT8U fl_total_task = sizeof(task_para)/sizeof(TASK_PARA_ST);
+
+	for(fl_index = 1; fl_index < fl_total_task; ++fl_index)
+	{
+    OSTaskCreateExt(task_para[fl_index].task,
                     (void *)0,
-                    &Task1MSStk[TASK_STK_SIZE-1],
-                    TASK_1MS_PRIO,
-                    TASK_1MS_ID,
-                    &Task1MSStk[0],
-                    TASK_STK_SIZE,
-                    (void *)0,
-                    OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
-    OSTaskCreateExt(Task10MS,
-                    (void *)0,
-                    &Task10MSStk[TASK_STK_SIZE-1],
-                    TASK_10MS_PRIO,
-                    TASK_10MS_ID,
-                    &Task10MSStk[0],
-                    TASK_STK_SIZE,
+										task_para[fl_index].ptos,
+										task_para[fl_index].prio,
+										task_para[fl_index].id,
+										task_para[fl_index].pbos,
+										task_para[fl_index].stk_size,
                     (void *)0,
                     OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
-    OSTaskCreateExt(Task20MS,
-                    (void *)0,
-                    &Task20MSStk[TASK_STK_SIZE-1],
-                    TASK_20MS_PRIO,
-                    TASK_20MS_ID,
-                    &Task20MSStk[0],
-                    TASK_STK_SIZE,
-                    (void *)0,
-                    OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
-    OSTaskCreateExt(Task50MS,
-                    (void *)0,
-                    &Task50MSStk[TASK_STK_SIZE-1],
-                    TASK_50MS_PRIO,
-                    TASK_50MS_ID,
-                    &Task50MSStk[0],
-                    TASK_STK_SIZE,
-                    (void *)0,
-                    OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
-    OSTaskCreateExt(Task100MS,
-                    (void *)0,
-                    &Task100MSStk[TASK_STK_SIZE-1],
-                    TASK_100MS_PRIO,
-                    TASK_100MS_ID,
-                    &Task100MSStk[0],
-                    TASK_STK_SIZE,
-                    (void *)0,
-                    OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
-    OSTaskCreateExt(Task500MS,
-                    (void *)0,
-                    &Task500MSStk[TASK_STK_SIZE-1],
-                    TASK_500MS_PRIO,
-                    TASK_500MS_ID,
-                    &Task500MSStk[0],
-                    TASK_STK_SIZE,
-                    (void *)0,
-                    OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
-    OSTaskCreateExt(Task1KMS,
-                    (void *)0,
-                    &Task1KMSStk[TASK_STK_SIZE-1],
-                    TASK_1KMS_PRIO,
-                    TASK_1KMS_ID,
-                    &Task1KMSStk[0],
-                    TASK_STK_SIZE,
-                    (void *)0,
-                    OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
-    OSTaskCreateExt(TaskStkChk,
-                    (void *)0,
-                    &TaskStkChkStk[TASK_STK_SIZE-1],
-                    TASK_STK_CHK_PRIO,
-                    TASK_STK_CHK_ID,
-                    &TaskStkChkStk[0],
-                    TASK_STK_SIZE,
-                    (void *)0,
-                    OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
+	}
 }
 
 /*
@@ -311,10 +290,19 @@ void Task500MS(void *pdata)
 {
 	pdata = pdata;
   
+	GPKDAT_REG |= 0x20;
+	GPKCON0_REG = (GPKCON0_REG & ~0x00f00000) | 0x00100000;
 	printf("Task500MS started!\r\n");
 
 	for(;;) {
-		//printf("Task 500MS going!!!\r\n");
+		if((GPKDAT_REG & 0x20) == 0)
+		{
+			GPKDAT_REG |= 0x20;
+		}
+		else
+		{
+			GPKDAT_REG &= ~0x20;
+		}
 		OSTimeDly(OS_TICKS_PER_SEC/2);
 	}
 }
@@ -328,10 +316,19 @@ void Task1KMS(void *pdata)
 {
 	pdata = pdata;
   
+	GPKDAT_REG |= 0x10;
+	GPKCON0_REG = (GPKCON0_REG & ~0x000f0000) | 0x00010000;
 	printf("Task1KMS started!\r\n");
 
 	for(;;) {
-		printf("Task 1KMS going!!!\r\n");
+		if((GPKDAT_REG & 0x10) == 0)
+		{
+			GPKDAT_REG |= 0x10;
+		}
+		else
+		{
+			GPKDAT_REG &= ~0x10;
+		}
 		OSTimeDly(OS_TICKS_PER_SEC);
 	}
 }
@@ -343,23 +340,20 @@ void Task1KMS(void *pdata)
 */
 void TaskStkChk(void *pdata)
 {
-  OS_STK_DATA idle_data, stkchk_data;
-#if OS_TASK_STAT_EN > 0
-	OS_STK_DATA stat_data; 
-#endif
-  
+	INT8U fl_index = 0;
+	INT8U fl_total_task = sizeof(task_para)/sizeof(TASK_PARA_ST);
+
   pdata = pdata;
   
 	printf("TaskStkChk started!\r\n");
 
   for(;;) {
-        
-		printf("Task stack check going!!!\r\n");
-    OSTaskStkChk(63, &idle_data);
-	#if OS_TASK_STAT_EN > 0
-    OSTaskStkChk(62, &stat_data);
-	#endif
-    OSTaskStkChk(TASK_STK_CHK_PRIO, &stkchk_data);
+		for(fl_index = 0; fl_index < fl_total_task; ++fl_index)
+		{
+			OSTaskStkChk(task_para[fl_index].prio, &task_para[fl_index].stk_chk_data);
+			printf("[%02d] used: %4d, free: %4d\r\n", task_para[fl_index].id, \
+					task_para[fl_index].stk_chk_data.OSUsed, task_para[fl_index].stk_chk_data.OSFree);
+		}
     OSTimeDly(OS_TICKS_PER_SEC);
   }
 }
